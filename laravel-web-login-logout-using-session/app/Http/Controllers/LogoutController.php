65658639170c2e88f8ac7c4431d3c18078e01e1b
thinhcubi <thinhcubi27@gmail.com>
2021-07-27 15:03:31 +0700

    laravel-web-login-logout-using-session

---
+++ laravel-web-login-logout-using-session/app/Http/Controllers/LogoutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function logout(Request $request)
+    {
+        // Xóa Session login, đưa người dùng về trạng thái chưa đăng nhập
+        $request->session()->flush();
+        return redirect('login');
+    }
+
+}
